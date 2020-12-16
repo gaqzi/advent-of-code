539291e9c8b20df3ac9d2f70eba7cd2fc8a1dba6
Björn Andersson <ba@sanitarium.se>
2020-12-16 22:35:09 +0800

    Extract common collection logic
    Not 100% sure of using callback for counting, but it does highlight
    what is the part that is different. Particularly, I'm not super keen on
    giving the number of people as an input. But since it's all I have
    right now it seems good enough .Better than passing the full group.

--- day06/exercise.go
+++ day06/exercise.go
@@ -1,22 +1,26 @@
 package day06
 
 func UniqueAnswers(groups [][]string) int {
-	var total int
-	for _, group := range groups {
-		answers := map[int32]struct{}{}
-		for _, person := range group {
-			for _, answer := range person {
-				answers[answer] = struct{}{}
-			}
-		}
-
-		total += len(answers)
-	}
-
-	return total
+	return reduce(groups, func(_ int, answers map[int32]int) int {
+		return len(answers)
+	})
 }
 
 func AllSameInGroup(groups [][]string) int {
+	return reduce(groups, func(people int, answers map[int32]int) int {
+		var total int
+
+		for _, responders := range answers {
+			if responders == people {
+				total++
+			}
+		}
+
+		return total
+	})
+}
+
+func reduce(groups [][]string, counter func(people int, answers map[int32]int) int) int {
 	var total int
 
 	for _, group := range groups {
@@ -27,11 +31,7 @@ func AllSameInGroup(groups [][]string) int {
 			}
 		}
 
-		for _, v := range answers {
-			if v == len(group) {
-				total++
-			}
-		}
+		total += counter(len(group), answers)
 	}
 
 	return total
